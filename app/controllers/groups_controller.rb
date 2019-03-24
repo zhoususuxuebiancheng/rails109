7ff27861c92c898ad8dceb809bd610948714163d
gususu <zhoususuxuebiancheng@gmail.com>
2019-03-24 11:02:26 +0800

    validation

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -13,8 +13,11 @@ class GroupsController < ApplicationController
   end
   def create
     @group = Group.new(group_params)
-    @group.save
+  if  @group.save
     redirect_to groups_path
+  else
+    render :new
+  end 
 end
 def update
   @group = Group.new(group_params)
