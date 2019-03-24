e76b1f2a5c448b9069f342408b920f840488b2ee
gususu <zhoususuxuebiancheng@gmail.com>
2019-03-24 10:57:35 +0800

    destroy

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -20,7 +20,14 @@ def update
   @group = Group.new(group_params)
   @group.update(group_params)
   redirect_to groups_path, notice: "Update Success"
-end 
+end
+def destroy
+  @group = Group.find(params[:id])
+  @group.destroy
+  flash[:alert] = "Group deleted"
+  redirect_to groups_path
+end
+
  private
  def group_params
     params.require(:group).permit(:title, :description)
