269011bf8d14f4b4213693b25a32d5c03100e4e8
gususu <zhoususuxuebiancheng@gmail.com>
2019-03-24 12:44:02 +0800

    post funtion

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -7,6 +7,7 @@ class GroupsController < ApplicationController
   end
   def show
     @group = Group.find(params[:id])
+    @posts = @group.posts
   end
   def edit
 
@@ -33,7 +34,7 @@ def update
     end
   end
   def destroy
-      
+
 
       @group.destroy
       redirect_to groups_path, alert: "Group deleted"
