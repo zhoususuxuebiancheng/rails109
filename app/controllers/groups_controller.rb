e19e8f04337d59efe7cf1a77376a473de3e8b947
gususu <zhoususuxuebiancheng@gmail.com>
2019-03-24 10:49:55 +0800

    show

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,6 +2,9 @@ class GroupsController < ApplicationController
   def index
     @groups=Group.all
   end
+  def show
+    @group = Group.find(params[:id])
+  end
   def new
     @group = Group.new
   end
@@ -12,6 +15,6 @@ class GroupsController < ApplicationController
 end
  private
  def group_params
-   params.require(:group).permit(:title, :description)
- end
+    params.require(:group).permit(:title, :description)
+  end
 end
