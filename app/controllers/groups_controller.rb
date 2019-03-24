23da06a7859853a296894ac06b4c6faa7a2e3ec2
gususu <zhoususuxuebiancheng@gmail.com>
2019-03-24 12:17:22 +0800

    check permission and hide eidt

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user! , only: [:new, :create]
+  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
   def index
     @groups=Group.all
   end
@@ -8,6 +8,10 @@ class GroupsController < ApplicationController
   end
   def edit
     @group = Group.find(params[:id])
+
+    if current_user != @group.user
+      redirect_to root_path, alert: "You have no permission."
+    end
   end
   def new
     @group = Group.new
@@ -22,20 +26,28 @@ class GroupsController < ApplicationController
   end
 end
 def update
-  @group = Group.find(params[:id])
-if @group = Group.update(group_params)
+    @group = Group.find(params[:id])
 
-  redirect_to groups_path, notice: "Update Success"
-else
-  render :edit
-end
-end
-def destroy
-  @group = Group.find(params[:id])
-  @group.destroy
-  flash[:alert] = "Group deleted"
-  redirect_to groups_path
-end
+    if current_user != @group.user
+      redirect_to root_path, alert: "You have no permission."
+    end
+
+    if @group.update(group_params)
+      redirect_to groups_path, notice: "Update Success"
+    else
+      render :edit
+    end
+  end
+  def destroy
+      @group = Group.find(params[:id])
+
+      if current_user != @group.user
+        redirect_to root_path, alert: "You have no permission."
+      end
+
+      @group.destroy
+      redirect_to groups_path, alert: "Group deleted"
+    end
 
  private
  def group_params
